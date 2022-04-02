ba88fb40fd5be984e6d863dbf07426470c0b1a0f
elijahomolo <eomolo11@gmail.com>
2022-04-02 10:46:39 -0400

    established connection to db

---
+++ myapp/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+// load .env file
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+// connect to the database and return it as an object
+func dbConn() (db *sql.DB) {
+	// pass the db credentials into variables
+	host := goDotEnvVariable("DBHOST")
+	port := goDotEnvVariable("DBPORT")
+	dbUser := goDotEnvVariable("DBUSER")
+	dbPass := goDotEnvVariable("DBPASS")
+	dbname := goDotEnvVariable("DBNAME")
+	// create a connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, dbUser, dbPass, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+// print Connected! if connection is successful
+func main() {
+	db := dbConn()
+	var err error
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
